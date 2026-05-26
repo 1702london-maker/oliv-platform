e22bc3b7e72974cc3781f6a922fe17aea699034d
1702london-maker <1702london@gmail.com>
2026-05-26 22:16:05 +0100

    Grant service role order tracking access

---
+++ supabase/migrations/20260526204500_order_service_role_grants.sql
@@ -0,0 +1,7 @@
+grant all on orders to service_role;
+grant all on order_items to service_role;
+grant all on wholesale_accounts to service_role;
+grant all on affiliates to service_role;
+grant all on affiliate_commissions to service_role;
+grant all on affiliate_clicks to service_role;
+grant all on affiliate_payouts to service_role;
